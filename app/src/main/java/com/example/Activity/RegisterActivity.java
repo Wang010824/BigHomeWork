314eac38294f5fc78d85a987dafce7ee4cc31618
xiaoxiongxubaba <2993960464@qq.com>
2022-05-24 22:46:18 +0800

    登录，注册界面，输入框输入改为只能输入单行，密码隐藏

--- app/src/main/java/com/example/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/Activity/RegisterActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.DataBase.DBUtil;
 import com.example.myapplication.R;
+
 //注册页面
 public class RegisterActivity extends AppCompatActivity {
     public Button rsgister_button;
@@ -17,35 +18,43 @@ public class RegisterActivity extends AppCompatActivity {
     public EditText rsgister_password2;
     public EditText rsgister_username;
     public EditText rsgister_phone;
-    public DBUtil dbutil=new DBUtil();
+    public DBUtil dbutil = new DBUtil();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        rsgister_button=findViewById(R.id.rsgister_button);
-        rsgister_password1=findViewById(R.id.rsgister_password1);
-        rsgister_password2=findViewById(R.id.rsgister_password2);
-        rsgister_username=findViewById(R.id.rsgister_username);
-        rsgister_phone=findViewById(R.id.rsgister_phone);
+        rsgister_button = findViewById(R.id.rsgister_button);
+        rsgister_password1 = findViewById(R.id.rsgister_password1);
+        rsgister_password2 = findViewById(R.id.rsgister_password2);
+        rsgister_username = findViewById(R.id.rsgister_username);
+        rsgister_phone = findViewById(R.id.rsgister_phone);
 
         rsgister_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String buttonusername=rsgister_username.getText().toString();
-                String buttonpassword1=rsgister_password1.getText().toString();
-                String buttonpassword2=rsgister_password2.getText().toString();
-                String buttonphone=rsgister_phone.getText().toString();
-                if(buttonpassword1.equals(buttonpassword2)){
-                    if(dbutil.register(buttonusername,buttonphone,buttonpassword1)){
-                        Toast.makeText(RegisterActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+                String buttonusername = rsgister_username.getText().toString();
+                String buttonpassword1 = rsgister_password1.getText().toString();
+                String buttonpassword2 = rsgister_password2.getText().toString();
+                String buttonphone = rsgister_phone.getText().toString();
+                if (buttonusername.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "用户名不可为空", Toast.LENGTH_SHORT).show();
+                } else if (buttonphone.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "请输入手机号", Toast.LENGTH_SHORT).show();
+                } else if (buttonpassword1.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "密码不可为空", Toast.LENGTH_SHORT).show();
+                } else if (buttonpassword2.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "请再次输入密码", Toast.LENGTH_SHORT).show();
+                } else if (buttonpassword1.equals(buttonpassword2)) {
+                    if (dbutil.register(buttonusername, buttonphone, buttonpassword1)) {
+                        Toast.makeText(RegisterActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
                         finish();
-                    }else{
-                        Toast.makeText(RegisterActivity.this,"该用户已存在",Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "该用户已存在", Toast.LENGTH_SHORT).show();
                     }
-                } else{
-                    Toast.makeText(RegisterActivity.this,"两次输入的密码不一致",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "两次输入的密码不一致", Toast.LENGTH_SHORT).show();
                 }
             }
         });
